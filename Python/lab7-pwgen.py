0af2b6ffa92f45964222043cb2bed9e5a7d4c63f
kylegdouglas <kyle.dgls@gmail.com>
2017-10-03 16:23:48 -0700

    first draft

---
+++ Python/lab7-pwgen.py
@@ -0,0 +1,12 @@
+import random
+
+print('Welcome to the password generator')
+
+x = '123456789ABCDEFGHIJKLMNOPQURSTUV!@#$%^&*()'
+i = 0
+password = ''
+while i < 12:
+    password += (random.choice(x))
+    i += 1
+random.choice(x)
+print(password)
