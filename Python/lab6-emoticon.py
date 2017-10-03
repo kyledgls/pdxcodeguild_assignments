22d1e44fbaa2fd04e244ecc3173d6a8bcdda6355
kylegdouglas <kyle.dgls@gmail.com>
2017-10-03 15:59:52 -0700

    first draft

---
+++ Python/lab6-emoticon.py
@@ -0,0 +1,10 @@
+import random
+print('Welcome to the EMOTE Jungle')
+eyes = [';', ':', '8']
+nose = ['<', '>', 'U']
+mouth = [')', '(', "P"]
+
+emote = (random.choice(eyes))
+dmote = (random.choice(nose))
+vmote = (random.choice(mouth))
+print(emote, dmote, vmote)
