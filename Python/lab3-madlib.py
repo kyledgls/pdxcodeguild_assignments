ea54fbca0c5e5e00ce5f996e19022d2353c5a1e1
kylegdouglas <kyle.dgls@gmail.com>
2017-10-03 14:49:31 -0700

    first draft

---
+++ Python/lab3-madlib.py
@@ -0,0 +1,23 @@
+print ("Welcome to my MADLIBS!")
+
+adjective = input('Adjective: ')
+princess = input('Princess name: ')
+number = input('Number: ')
+relative = input('Female relative ')
+flee = input('Place:')
+hide = input('Hiding spot: ')
+verb = input('Verb: ')
+plnoun = input('Plural noun: ')
+adjective2 = input('adjective: ')
+hndadj = input('Handsome adjective: ')
+prince = input('Hero name: ')
+adly = input('Adjective/-ly: ')
+
+reply_line1 = 'A new and ' + adjective + ' fairy princess movie is coming out soon! it will be about Snow ' + princess +'\n'
+reply_line2 = 'and the '+ number + ' dwarfs. Snow '+ princess + ' is a princess who\'s beauty threatens her '+ relative +'\n'
+reply_line3 = ', the queen. Snow '+ princess + ' is forced to flee from '+ flee + ' and hides nearby in '+ hide +'\n'
+reply_line4 = 'There, she discovers the dwarfs '+ verb +' in their '+ plnoun + '. But the Queen finds her and casts \n'
+reply_line5 = 'a'+ adjective2 + ' spell on her. The dwarfs take care of her until the '+ hndadj + prince + ' comes to \n'
+reply_line6 = 'rescue her, and they live '+ adly + ' ever after! '
+
+print(reply_line1, reply_line2, reply_line3, reply_line4, reply_line5, reply_line6)
