a400ce042a3a001eafac875fc68af95482b39795
kylegdouglas <kyle.dgls@gmail.com>
2017-10-04 12:05:35 -0700

    first draft

--- Python/lab9-change.py
+++ Python/lab9-change.py
@@ -1,11 +1,13 @@
 '''
 Lets make change
 '''
-
+# welcomes user to program
 print('Let\'s make CHANGE!')
 
+# inputs the amount and stores it
 cents = input('How many pennies ' )
 cents = int(cents)
+# calculates the change
 quarters = (cents // 25)
 cents -= quarters * 25
 dimes = (cents // 10)
@@ -14,4 +16,5 @@ nickle = (cents // 5)
 cents -= (nickle * 5)
 penny = (cents // 1)
 cents -= (penny * 1)
+# prints the result
 print( 'Quarters', + quarters  ,'dimes',  dimes , 'nickles', nickle ,'pennies', penny)
