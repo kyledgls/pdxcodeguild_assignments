6512be551aa8f69132ad1f94809c2d4f364824fa
kylegdouglas <kyle.dgls@gmail.com>
2017-10-04 12:14:19 -0700

    first draft

---
+++ Python/lab10-unitcon.py
@@ -0,0 +1,15 @@
+'''
+welcome to my unit conversion program.
+'''
+# welcomes user to program
+print('Welcome to the feet to meters conversion program')
+# inputs the original length
+feet = input('How many feet?' )
+feet = int(feet)
+# converts to meter
+meters = (feet * 0.3048)
+# prints results
+print(meters, 'M')
+
+
+
