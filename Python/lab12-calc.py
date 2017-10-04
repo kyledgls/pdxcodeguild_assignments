379f3a7f30094d62984adf348a5164eb565c0a75
kylegdouglas <kyle.dgls@gmail.com>
2017-10-04 14:28:56 -0700

    first draft

--- Python/lab12-calc.py
+++ Python/lab12-calc.py
@@ -1,17 +1,22 @@
 
 print('Welcome to a basic calculator')
-operator = input("CHOOSE ONE * + / - : ")
-first = int(input('SELECT FIRST NUMBER: '))
-second = int(input('SELECT SECOND NUMBER: '))
-float(first)
-float(second)
-if operator == '+':
-    print(first, "+", second, "=", first + second)
-elif operator == '-':
-    print(first, "-", second, "=", first - second)
-elif operator == "*":
-    print(first, "*", second, "=", first * second)
-elif operator == "/":
-    print(first, "/", second, "=", first / second)
-else:
-    print('ERROR')
+while  True:
+
+    operator = input("CHOOSE ONE * + / - : ")
+    first = int(input('SELECT FIRST NUMBER: '))
+    second = int(input('SELECT SECOND NUMBER: '))
+    float(first)
+    float(second)
+    if operator == '+':
+        print(first, "+", second, "=", first + second)
+    elif operator == '-':
+        print(first, "-", second, "=", first - second)
+    elif operator == "*":
+        print(first, "*", second, "=", first * second)
+    elif operator == "/":
+        print(first, "/", second, "=", first / second)
+    else:
+        print('ERROR')
+    done = input('are you done? type DONE.: ')
+    if done == 'done':
+        break
