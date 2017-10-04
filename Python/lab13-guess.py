dacb420e9f9da2db240fe5e53d754d64a983c161
kylegdouglas <kyle.dgls@gmail.com>
2017-10-04 15:14:38 -0700

    first draft

---
+++ Python/lab13-guess.py
@@ -0,0 +1,16 @@
+import random
+x = random.randint(1,10)
+last_guess = 0
+while True:
+    guess = int(input('Make a guess: '))
+    if guess == x:
+        print('Well done!!')
+        break
+    elif guess != x:
+        print('Failure will not be tolerated')
+        current_difference = abs(guess - x)
+        last_difference = abs(last_guess - x)
+        if current_difference > last_difference:
+            print('Getting colder')
+        elif current_difference < last_difference:
+            print('Getting Warmer')
