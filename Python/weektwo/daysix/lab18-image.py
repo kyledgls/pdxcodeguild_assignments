6c6cdb9d89b92d49bdda61e2fe0fffc04d7d76c5
kylegdouglas <kyle.dgls@gmail.com>
2017-10-16 11:54:13 -0700

    second draft

--- Python/weektwo/daysix/lab18-image.py
+++ Python/weektwo/daysix/lab18-image.py
@@ -1,7 +1,7 @@
 from PIL import Image
 import colorsys
 
-img = Image.open("daysix/Lenna.png") # must be in same folder
+img = Image.open("Lenna.png") # must be in same folder
 width, height = img.size
 pixels = img.load()
 
@@ -9,9 +9,9 @@ for i in range(width):
     for j in range(height):
         r, g, b = pixels[i, j]
         h, s, v = colorsys.rgb_to_hsv(r / 255, g / 255, b / 255)
-        r = int(.1*r)
-        g = int(.1*g)
-        b = int(.1*b)
+        r = int(255*r)
+        g = int(255*g)
+        b = int(255*b)
 
         pixels[i, j] = (r, g, b)
 
