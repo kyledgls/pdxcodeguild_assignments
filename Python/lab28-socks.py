16441a9627c63e9170d2e89c6c16bad912af4ef2
KyleDgls <kyle.dgls@gmail.com>
2017-10-26 12:28:53 -0700

    first draft

--- Python/lab28-socks.py
+++ Python/lab28-socks.py
@@ -5,6 +5,7 @@ original version by Kyle Douglas
 import random
 
 sock_types = ['ankle', 'crew', 'calf', 'thigh']
+
 i = 0
 laundry = []
 while i < 100:
@@ -12,8 +13,23 @@ while i < 100:
     i += 1
 print(laundry)
 
+sorted_laundry = {}
+lone_rangers = {}
+for sock in laundry:
+    if sock not in sorted_laundry:
+        sorted_laundry[sock] = 1
+    else:
+        sorted_laundry[sock] += 1
+
+
+for sock_type in sorted_laundry:
+    if sorted_laundry[sock_type]%2 == 1:
+        lone_rangers[sock_type] = 1
 
 
 
+
+print(sorted_laundry)
+print(lone_rangers)
 
 
