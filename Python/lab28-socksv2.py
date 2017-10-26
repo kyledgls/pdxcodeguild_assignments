641e1fc480a06048e4b70923200db8f75bf7f4d5
KyleDgls <kyle.dgls@gmail.com>
2017-10-26 12:29:07 -0700

    first draft

---
+++ Python/lab28-socksv2.py
@@ -0,0 +1,34 @@
+"""
+sock sorter
+original version by Kyle Douglas
+"""
+import random
+
+sock_types = [('ankle', 'Blue'), ('crew', 'Black'), ('calf','White'), ('thigh', 'red')]
+
+i = 0
+laundry = []
+while i < 100:
+    laundry.append(random.choice(sock_types))
+    i += 1
+print(laundry)
+
+sorted_laundry = {}
+lone_rangers = {}
+for sock in laundry:
+    if sock not in sorted_laundry:
+        sorted_laundry[sock] = 1
+    else:
+        sorted_laundry[sock] += 1
+
+
+for sock_type in sorted_laundry:
+    if sorted_laundry[sock_type]%2 == 1:
+        lone_rangers[sock_type] = 1
+
+
+
+print(sorted_laundry)
+print(lone_rangers)
+
+
